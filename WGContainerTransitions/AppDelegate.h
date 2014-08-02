99191cd820be4e61b237d250c9909d0b1fc8ca00
wanggang <wgang316@163.com>
2014-08-02 21:44:09 +0800

    Initial Commit

---
+++ WGContainerTransitions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGContainerTransitions
+//
+//  Created by 王刚 on 14/8/2.
+//  Copyright (c) 2014年 wwwlife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
